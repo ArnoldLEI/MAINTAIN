2b5b996bc1a74065dfffbac94c66449f0d659a3f
雷以諾 <ae865566@gmail.com>
2026-07-02 23:21:56 +0800

    fix: make start.bat ASCII-only to prevent Windows CMD encoding issues

--- start.bat
+++ start.bat
@@ -1,16 +1,6 @@
 @echo off
 title MaintainSYS Starter
-echo ===================================================
-echo   正在啟動 MaintainSYS 維護系統...
-echo ===================================================
-echo.
-
+echo Starting MaintainSYS...
 cd /d "e:\MaintainSYS"
-
-:: 在背景等待 2 秒後自動在瀏覽器中開啟系統網址
 start "" "http://localhost:5173/"
-
-:: 啟動 Vite 開發伺服器
 call npm run dev
-
-pause
